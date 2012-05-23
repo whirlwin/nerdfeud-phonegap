78da33a3cc3b0017ccf6189926cfcc7f75247cd7
whirlwin <whirlwin90@gmail.com>
2012-05-23 15:39:50 +0200

    Cleaned up.

--- src/no/whirlwin/nerdfeud/Main.java
+++ src/no/whirlwin/nerdfeud/Main.java
@@ -1,13 +1,20 @@
 package no.whirlwin.nerdfeud;
 
+import org.apache.cordova.DroidGap;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class NerdfeudActivity extends Activity {
-    /** Called when the activity is first created. */
+/**
+ * 
+ * @author whirlwin
+ *
+ */
+public class Main extends DroidGap {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
