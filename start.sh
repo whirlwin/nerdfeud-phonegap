22e9ea1b884d57e2e4eefca0582451a4b19fdf1f
whirlwin <whirlwin90@gmail.com>
2012-05-24 18:27:46 +0200

    Added libraries.

--- start.sh
+++ start.sh
@@ -5,10 +5,7 @@ ruby tools/file-converter.rb
 echo Uninstalling Nerdfeud:
 adb -d uninstall no.whirlwin.nerdfeud
 
-ant debug
-
-echo Installing Nerdfeud:
-adb -d install bin/nerdfeud-debug.apk
+ant debug install
 
 echo Starting Nerdfeud:
 adb -d shell am start -n no.whirlwin.nerdfeud/.Main
