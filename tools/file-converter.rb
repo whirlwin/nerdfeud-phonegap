53d177664953728ecb361867550b7e783df57622
whirlwin <whirlwin90@gmail.com>
2012-05-29 11:26:17 +0200

    Renamed files and altered the script to it.

--- tools/file-converter.rb
+++ tools/file-converter.rb
@@ -6,7 +6,7 @@ Dir.glob '**/*' do |haml_file|
   if (File.extname(haml_file) == '.haml')
     puts 'Converting Haml to HTML: ' + haml_file    
     File.write(
-        haml_file.chomp(File.extname(haml_file)),
+        haml_file.sub(/(.)\.haml/, '\1.html'),
         Haml::Engine.new(File.open(haml_file).read).render)
   end
 end
