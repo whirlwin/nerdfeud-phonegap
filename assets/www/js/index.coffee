6979f3461538833323e148483f9460d95d7d4e94
whirlwin <whirlwin90@gmail.com>
2012-05-30 10:06:10 +0200

    Began added CoffeeScript support.

---
+++ assets/www/js/index.coffee
@@ -0,0 +1,2 @@
+$(document).on 'pageinit' ->
+  alert 'foobar'
--- tools/file-converter.rb
+++ tools/file-converter.rb
@@ -1,12 +1,28 @@
 #!/usr/lib/ruby
 
+require 'coffee-script'
 require 'haml'
 
 Dir.glob '**/*' do |haml_file|
-  if (File.extname(haml_file) == '.haml')
+  case File.extname haml_file
+  when '.coffee'
+    puts "Converting #{haml_file} to JavaScript"
+  when '.haml'
+    puts "Converting #{haml_file} to HTML"
+  end
+end
+
+def write_to_file(file_name, contents)
+  File.write()
+end
+
+=begin
+  if 1==1 exit
+  if (File.extname(haml_file) =~ /\.haml/)
     puts 'Converting Haml to HTML: ' + haml_file    
     File.write(
         haml_file.sub(/(.)\.haml/, '\1.html'),
         Haml::Engine.new(File.open(haml_file).read).render)
   end
 end
+=end
