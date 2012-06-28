b0fea86e55e03269551b37c7666b1caea9bade45
Øyvind Ødegård <whirlwin90@gmail.com>
2012-06-28 22:48:45 +0200

    Improved formatting.

--- tools/file-converter.rb
+++ tools/file-converter.rb
@@ -10,7 +10,7 @@ $file_exts = { 'haml' => 'html', 'coffee' => 'js' }
 def fwrite(name, contents)
   name.scan /(.+)\.(.+)/ do |base, orig_ext|
     new_ext = $file_exts[orig_ext]
-    puts "Converting #{name} to #{new_ext}"
+    puts "Converted #{name} to #{new_ext}"
     File.write("#{base}.#{new_ext}", contents)
   end
 end
@@ -18,8 +18,8 @@ end
 Dir.glob '**/*' do |file|
   case File.extname file
   when '.coffee'
-    fwrite file, CoffeeScript.compile(File.read(file))
+    fwrite file, CoffeeScript.compile(File.read file )
   when '.haml'
-    fwrite file, Haml::Engine.new(File.read(file)).render
+    fwrite file, Haml::Engine.new(File.read file).render
   end
 end
